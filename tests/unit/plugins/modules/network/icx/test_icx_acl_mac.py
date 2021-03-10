50698d1ae9f241c11645f9edf9265039334494a1
commscope123$ <thamaraiselvan.gajendran@commscope.com>
2021-03-10 09:16:39 +0530

    Add_Icx_acl_mac

---
+++ tests/unit/plugins/modules/network/icx/test_icx_acl_mac.py
@@ -0,0 +1,84 @@
+# Copyright: (c) 2019, Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from ansible_collections.community.network.tests.unit.compat.mock import patch
+from ansible_collections.community.network.plugins.modules.network.icx import icx_acl_mac
+from ansible_collections.community.network.tests.unit.plugins.modules.utils import set_module_args
+from .icx_module import TestICXModule, load_fixture
+
+class TestICXAclMacModule(TestICXModule):
+    """ Class used for Unit tests agains icx_acl_mac module"""
+    class TestICXBannerModule(TestICXModule):
+
+    module = icx_acl_mac
+
+    def setUp(self):
+        super(TestICXAclMacModule, self).setUp()
+        self.mock_load_config = patch('ansible_collections.community.network.plugins.modules.network.icx.icx_acl_mac.load_config')
+        self.load_config = self.mock_load_config.start()
+        self.mock_exec_command = patch('ansible_collections.community.network.plugins.modules.network.icx.icx_acl_mac.exec_command')
+        self.exec_command = self.mock_exec_command.start()
+
+    def tearDown(self):
+        super(TestICXAclMacModule, self).tearDown()
+        self.mock_load_config.stop()
+        self.mock_exec_command.stop()
+
+    def load_fixtures(self, commands=None): 
+	    self.load_config.return_value = None
+
+    def test_icx_acl_mac_all_options(self):
+        ''' Test for successful acl  with all options'''
+        set_module_args(dict(acl_name='mac123',
+                             rule=[(dict(rule_type='permit',source=dict(any='yes'),destination=dict(any='yes'), ether_type='0800', log='yes', mirror='yes'))
+                                   (dict(rule_type='permit',source=dict(source_mac_address='1111.2222.3333', source_mask='ffff.ffff.ffff'),destination=dict(destination_address='4444.5555.6666', destination_mask='ffff.ffff.ffff'), ether_type='0800', log='yes', mirror=yes))
+                                   (dict(rule_type='permit',source=dict(source_mac_address='1111.2222.3333', source_mask='ffff.ffff.ffff'), any='yes', ether_type='0800', log='yes', mirror='yes'))]))
+        expected_commands = ['mac access-list mac123',
+                             'permit any any ether_type 0800 log mirror',     
+                             'permit 1111.2222.3333 ffff.ffff.ffff 4444.5555.6666 ffff.ffff.ffff.ffff ether_type 0800 log mirror',
+                             'permit 1111.2222.3333 ffff.ffff.ffff any ether_type 0800 log mirror'        
+                             ]
+        result = self.execute_module(changed=True)
+        self.assertEqual(result['commands'], expected_commands)
+        
+    def test_icx_acl_mac_all_options_remove(self):
+        ''' Test for removing acl mac with all options'''
+        set_module_args(dict(acl_name='mac123',state='absent'
+                        rule=[(dict(rule_type='permit',source=dict(any='yes'),destination=dict(any='yes'), ether_type='0800', log='yes', mirror='yes'))
+                                   (dict(rule_type='permit',source=dict(source_mac_address='1111.2222.3333', source_mask='ffff.ffff.ffff'),destination=dict(destination_address='4444.5555.6666', destination_mask='ffff.ffff.ffff'), ether_type='0800', log='yes', mirror='yes'))
+                                   (dict(rule_type='permit',source=dict(source_mac_address='1111.2222.3333', source_mask='ffff.ffff.ffff'), any='yes', ether_type='0800', log='yes', mirror='yes'))]))     
+        expected_commands = ['no mac access-list mac123',
+                             'no permit any any ether_type 0800 log mirror',     
+                             'no permit 1111.2222.3333 ffff.ffff.ffff 4444.5555.6666 ffff.ffff.ffff.ffff',
+                             'no permit 1111.2222.3333 ffff.ffff.ffff any'        
+                             ]
+        result = self.execute_module(changed=True)
+        self.assertEqual(result['commands'], expected_commands)
+
+    def test_icx_mac_permit_acl(self):
+        ''' Test for successful mac rule permit acl'''
+        set_module_args(dict(acl_name='mac123'
+                             rule=[(dict(rule_type='permit',source=dict(source_mac_address='1111.2222.3333'),destination=dict(destination_address='4444.5555.6666'), ether_type='0800', log='yes', mirror:'yes'))]))
+        expected_commands = ['mac access-list mac123','permit 1111.2222.3333 ffff.ffff.ffff ether_type 0800 log mirror']
+        result = self.execute_module(changed=True)
+        self.assertEqual(result['commands'], expected_commands)
+
+    def test_icx_mac_deny_acl(self):
+        ''' Test for successful mac rule deny acl'''
+        set_module_args(dict(acl_name='mac123'
+                             rule=[(dict(rule_type='deny',source=dict(source_mac_address='1111.2222.3333'),destination=dict(destination_address='4444.5555.6666'), ether_type='0800', log='yes', mirror:'yes'))]))
+        expected_commands = ['mac access-list mac123','deny 1111.2222.3333 ffff.ffff.ffff ether_type 0800 log mirror']
+        result = self.execute_module(changed=True)
+        self.assertEqual(result['commands'], expected_commands)
+
+
+    def test_icx_invalid_mac_acl_ipv6_permit(self):
+        ''' Test for invalid acl_name'''
+        set_module_args(dict(acl_name='mac123',
+                             rule=[(dict(rule_type='permit',source=dict(source_mac_address='1111.2222.3333'),destination=dict(any='yes'),log='yes'))]))
+        result = self.execute_module(failed=True)
+
+
+
